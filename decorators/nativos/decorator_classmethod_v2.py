be730b4d8d30d8a42e45f85b303662e7ea001029
Eduardo Freitas <eduardoflorencio96@gmail.com>
2023-12-04 10:18:06 -0300

    refactor: rename file

---
+++ decorators/nativos/decorator_classmethod_v2.py
@@ -0,0 +1,12 @@
+class Utilidades:
+    """
+    @staticmethod + @classmethod:
+    Combinação de métodos de classe e métodos estáticos.
+    """
+
+    @classmethod
+    def exemplo(cls):
+        print("Exemplo")
+
+
+Utilidades.exemplo()  # Saída: Exemplo
